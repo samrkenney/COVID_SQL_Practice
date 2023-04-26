8765be62b7c9f813949ff97ed398a8eecd66591b
samrkenney <85599531+samrkenney@users.noreply.github.com>
2023-04-26 12:24:38 -0400

    Update COVID_practice_2g.sql

--- US_county_analysis/sql_queries/COVID_practice_2g.sql
+++ US_county_analysis/sql_queries/COVID_practice_2g.sql
@@ -5,12 +5,12 @@ SELECT x.County_ County,
        x.NCHS_Code Rurality_Score
   FROM
        (
-        SELECT A.County_name + '_' + A.State County_,
-               CAST(ISNULL(A.Deaths_involving_COVID_19,0) AS float) AS Total_COVID_Deaths,
-               (CAST(B._2020_Apr_1 AS float) + CAST(B._2020_Jul_1 AS float) + CAST(B._2021_Jul_1 AS float) + CAST(B._2022_Jul_1 AS float))/4 AS County_pop,
-               C._2013_code NCHS_Code
-          FROM COVID_practice_2.dbo.Provisional_COVID_19_Death_Counts_in_the_United_States_by_County A
-    INNER JOIN COVID_practice_2.dbo.co_est2022_pop B ON A.County_name + '_' + A.State = B.Geographic_Area
-    INNER JOIN COVID_practice_2.dbo.NCHSURCodes2013 C ON A.County_name + '_' + A.State = C.County_name + '_' + C.State_Abr
+            SELECT A.County_name + '_' + A.State County_,
+                   CAST(ISNULL(A.Deaths_involving_COVID_19,0) AS float) AS Total_COVID_Deaths,
+                   (CAST(B._2020_Apr_1 AS float) + CAST(B._2020_Jul_1 AS float) + CAST(B._2021_Jul_1 AS float) + CAST(B._2022_Jul_1 AS float))/4 AS County_pop,
+                   C._2013_code NCHS_Code
+              FROM COVID_practice_2.dbo.Provisional_COVID_19_Death_Counts_in_the_United_States_by_County A
+        INNER JOIN COVID_practice_2.dbo.co_est2022_pop B ON A.County_name + '_' + A.State = B.Geographic_Area
+        INNER JOIN COVID_practice_2.dbo.NCHSURCodes2013 C ON A.County_name + '_' + A.State = C.County_name + '_' + C.State_Abr
        ) x
 ORDER BY 2 DESC;
